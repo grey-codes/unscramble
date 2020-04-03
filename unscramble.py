6b0ef4653e29d42ce986606a475dd1862f3482ea
Grey Ruessler <greyruessler@gmail.com>
2020-04-02 22:48:04 -0500

    process edges

--- unscramble.py
+++ unscramble.py
@@ -19,3 +19,14 @@ for i in range(ROWS):
         y2 = y + segH
         chunk = img[x:x2,y:y2,0:imgL]
         chunkAr.append( {"chunk":chunk})
+for chunkTb in chunkAr:
+    chunk = chunkTb["chunk"]
+    chunkTb["L"]=chunk[0:1,0:segH,0:imgL]
+    chunkTb["R"]=chunk[segW-1:segW,0:segH,0:imgL]
+    chunkTb["T"]=chunk[0:segW,0:1,0:imgL]
+    chunkTb["B"]=chunk[0:segW,segH-1:segH,0:imgL]
+
+    #rotate orientation to match top
+    chunkTb["R"]=np.rot90(chunkTb["R"], 3)
+    chunkTb["B"]=np.rot90(chunkTb["B"], 2)
+    chunkTb["L"]=np.rot90(chunkTb["L"], 1)
